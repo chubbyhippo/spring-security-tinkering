562817dc46c658c178c9ae4ea4327f5f198efbc5
chubbyhippo <mkmsacc@outlook.com>
2024-12-08 17:40:06 +0700

    Add HelloController and corresponding test
    Introduce a simple REST controller with a "/hello" endpoint that returns a greeting message. Include a unit test to verify the controller's response status, content type, and body text.

---
+++ basic/src/test/java/io/github/chubbyhippo/basic/HelloControllerTest.java
@@ -0,0 +1,27 @@
+package io.github.chubbyhippo.basic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+@WebMvcTest
+@WithMockUser
+public class HelloControllerTest {
+    @Autowired
+    private MockMvcTester mockMvcTester;
+
+    @Test
+    @DisplayName("should return hello")
+    void shouldReturnHello() {
+        mockMvcTester.get()
+                .uri("/hello")
+                .assertThat()
+                .hasStatusOk()
+                .doesNotHaveFailed()
+                .hasContentType("text/plain;charset=UTF-8")
+                .hasBodyTextEqualTo("Hello!");
+    }
+}

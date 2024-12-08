c7a0d17b103fa51bb0bd2070896e6edd5f487c05
chubbyhippo <mkmsacc@outlook.com>
2024-12-08 17:32:16 +0700

    Initialize Spring Boot basic project setup
    This commit sets up a new Spring Boot project with Gradle as the build tool. It includes essential project files like build.gradle.kts, settings.gradle.kts, and necessary Gradle wrapper scripts. Additionally, configuration files for various IDEs are added to ensure proper project structure and dependencies management.

---
+++ basic/src/test/java/io/github/chubbyhippo/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.chubbyhippo.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

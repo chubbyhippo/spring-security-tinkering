a4db162bb8ecdd3a368a4688043ba0cd798b0982
chubbyhippo <mkmsacc@outlook.com>
2024-12-08 18:01:08 +0700

    Add dynamic encoding for BasicAuth credentials
    Replaced hardcoded Base64-encoded credentials in tests with dynamic encoding of username and password. This change enhances maintainability by computing credentials dynamically, and it makes updates to the username or password easier and less prone to errors.

--- basic/src/test/java/io/github/chubbyhippo/basic/BasicApplicationTests.java
+++ basic/src/test/java/io/github/chubbyhippo/basic/BasicApplicationTests.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+import java.util.Base64;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
 		args = "--spring.security.user.password=password" )
+@TestPropertySource(properties = "spring.security.user.name=hello")
 @AutoConfigureMockMvc
 class BasicApplicationTests {
 	@Autowired
@@ -17,9 +21,10 @@ class BasicApplicationTests {
 	@Test
 	@DisplayName("should return hello")
 	void shouldReturnHello() {
+		var base64 = Base64.getEncoder().encodeToString("hello:password".getBytes());
 		mockMvcTester.get()
 				.uri("/hello")
-				.header( "Authorization", "Basic dXNlcjpwYXNzd29yZA==")
+				.header( "Authorization", "Basic %s".formatted( base64))
 				.assertThat()
 				.hasStatusOk()
 				.doesNotHaveFailed()

75dc1a4b95703cb9d97a18cbe7dafb8030c383a2
chubbyhippo <mkmsacc@outlook.com>
2024-12-08 17:52:31 +0700

    Add MockMvc test for /hello endpoint with authentication
    Introduced a new test method `shouldReturnHello` in `BasicApplicationTests` to verify the /hello endpoint. The test ensures proper authentication with basic auth header and asserts expected response status, content type, and body. This enhancement improves test coverage for the application's security and endpoint functionality.

--- basic/src/test/java/io/github/chubbyhippo/basic/BasicApplicationTests.java
+++ basic/src/test/java/io/github/chubbyhippo/basic/BasicApplicationTests.java
@@ -1,13 +1,30 @@
 package io.github.chubbyhippo.basic;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+		args = "--spring.security.user.password=password" )
+@AutoConfigureMockMvc
 class BasicApplicationTests {
+	@Autowired
+	private MockMvcTester mockMvcTester;
 
 	@Test
-	void contextLoads() {
+	@DisplayName("should return hello")
+	void shouldReturnHello() {
+		mockMvcTester.get()
+				.uri("/hello")
+				.header( "Authorization", "Basic dXNlcjpwYXNzd29yZA==")
+				.assertThat()
+				.hasStatusOk()
+				.doesNotHaveFailed()
+				.hasContentType("text/plain;charset=UTF-8")
+				.hasBodyTextEqualTo("Hello!");
 	}
 
 }

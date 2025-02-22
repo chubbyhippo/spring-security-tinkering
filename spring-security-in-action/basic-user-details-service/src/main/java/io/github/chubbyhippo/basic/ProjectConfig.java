b5e5d7375fd1f15ad19b266a1d60682ddde205d5
chubbyhippo <mkmsacc@outlook.com>
2025-02-22 11:32:24 +0700

    Refactor user details setup in ProjectConfig.
    Moved userDetailsService configuration directly into the SecurityFilterChain bean. This streamlines the setup and ensures the user details service is explicitly tied to the security configuration. Unused code and comments were also cleaned up for better clarity.

--- spring-security-in-action/basic-user-details-service/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
+++ spring-security-in-action/basic-user-details-service/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -16,23 +15,29 @@ public class ProjectConfig {
 
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
         http.httpBasic(Customizer.withDefaults());
         http.authorizeHttpRequests(registry ->
-//                        registry.anyRequest().authenticated()
-                        registry.anyRequest().permitAll()
+                        registry.anyRequest().authenticated()
+//                        registry.anyRequest().permitAll()
         );
-        return http.build();
-    }
 
-    @Bean
-    UserDetailsService userDetailsService() {
         var user = User.withUsername("matthew")
                 .password("12345")
                 .authorities("read")
                 .build();
 
-        return new InMemoryUserDetailsManager(user);
+        var userDetailsService = new InMemoryUserDetailsManager(user);
+        http.userDetailsService(userDetailsService);
+
+        return http.build();
     }
+
+//    @Bean
+//    UserDetailsService userDetailsService() {
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
 
     @Bean
     PasswordEncoder passwordEncoder() {

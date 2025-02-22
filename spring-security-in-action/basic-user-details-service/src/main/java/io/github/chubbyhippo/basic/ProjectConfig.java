7483a194595d57a851fdfefbe819a6f05724b387
chubbyhippo <mkmsacc@outlook.com>
2025-02-22 10:43:12 +0700

    Add password encoder bean for in-memory authentication
    Configured a `PasswordEncoder` bean using `NoOpPasswordEncoder` to handle password encoding for the in-memory user details service. This ensures compatibility with the predefined user credentials and simplifies the authentication setup.

--- spring-security-in-action/basic-user-details-service/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
+++ spring-security-in-action/basic-user-details-service/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -17,6 +19,11 @@ public class ProjectConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+       return NoOpPasswordEncoder.getInstance();
     }
 
 }

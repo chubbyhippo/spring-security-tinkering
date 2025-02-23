ab079db017529744e0997ea70fd1ff6050ea2c4b
chubbyhippo <mkmsacc@outlook.com>
2025-02-23 13:25:41 +0700

    Update LDAP dependency to spring-security-ldap
    Replaced the spring-boot-starter-data-ldap dependency with spring-security-ldap. This ensures more focused and flexible LDAP support, aligning better with application requirements.

--- spring-security-in-action/basic-ldap-user-details-manager-embedded/build.gradle.kts
+++ spring-security-in-action/basic-ldap-user-details-manager-embedded/build.gradle.kts
@@ -18,9 +18,9 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-ldap")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.security:spring-security-ldap")
 	implementation("com.unboundid:unboundid-ldapsdk")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")

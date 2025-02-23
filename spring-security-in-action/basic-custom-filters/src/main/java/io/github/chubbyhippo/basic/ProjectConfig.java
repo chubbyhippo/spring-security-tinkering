cf2cf755ade926c7196d096b63d05af9202c1aeb
chubbyhippo <mkmsacc@outlook.com>
2025-02-23 19:49:24 +0700

    Add security filter chain configuration
    Introduce a `ProjectConfig` class to configure Spring Security. The configuration adds `RequestValidationFilter` before the `BasicAuthenticationFilter` and permits all requests for now. This sets up the foundation for customizing request validation logic.

---
+++ spring-security-in-action/basic-custom-filters/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
@@ -0,0 +1,22 @@
+package io.github.chubbyhippo.basic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.addFilterBefore(new RequestValidationFilter(),
+                        BasicAuthenticationFilter.class)
+                .authorizeHttpRequests(registry ->
+                        registry.anyRequest().permitAll());
+
+        return http.build();
+    }
+}

a5dbcb0be1dd02e63f871930f41ec1f1219645c5
chubbyhippo <mkmsacc@outlook.com>
2025-02-23 11:45:55 +0700

    Add JDBC UserDetailsManager and SecurityFilterChain setup
    Replaced in-memory user details service with JDBC-based implementation using `JdbcUserDetailsManager`. Introduced a `SecurityFilterChain` bean to configure HTTP basic authentication, disable CSRF for the H2 console, and allow frame options for it. Adjusted security rules to permit access to the H2 console while requiring authentication for other endpoints.

--- spring-security-in-action/basic-jdbc-user-details-manager-h2/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
+++ spring-security-in-action/basic-jdbc-user-details-manager-h2/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
@@ -2,23 +2,41 @@ package io.github.chubbyhippo.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.List;
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectConfig {
 
     @Bean
-    UserDetailsService userDetailsService() {
-        var user = new User("matthew", "12345", "read");
-        return new InMemoryUserDetailService(List.of(user));
+    UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable) // disable csrf for h2 console
+                .headers(configurer -> configurer
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)) // allow frames for h2 console
+                .authorizeHttpRequests(registry ->
+                        registry.requestMatchers("/h2-console/**").permitAll() // permit h2 console
+                                .anyRequest()
+                                .authenticated())
+                .build();
     }
 }

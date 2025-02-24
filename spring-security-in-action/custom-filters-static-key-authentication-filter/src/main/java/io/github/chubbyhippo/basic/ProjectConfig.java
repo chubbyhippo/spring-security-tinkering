bd1f3688f099a3051a73d7483e44ba1fb6738bac
chubbyhippo <mkmsacc@outlook.com>
2025-02-24 21:31:32 +0700

    Replace custom filters with StaticKeyAuthenticationFilter
    Simplified the filter configuration by removing RequestValidationFilter and AuthenticationLoggingFilter. Integrated StaticKeyAuthenticationFilter directly into the security filter chain for streamlined processing. This enhances maintainability and reduces complexity in filter management.

--- spring-security-in-action/custom-filters-static-key-authentication-filter/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
+++ spring-security-in-action/custom-filters-static-key-authentication-filter/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
@@ -1,7 +1,5 @@
 package io.github.chubbyhippo.basic;
 
-import io.github.chubbyhippo.basic.filters.AuthenticationLoggingFilter;
-import io.github.chubbyhippo.basic.filters.RequestValidationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,17 +8,18 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 
 @Configuration
 public class ProjectConfig {
+    private final StaticKeyAuthenticationFilter staticKeyAuthenticationFilter;
+
+    public ProjectConfig(StaticKeyAuthenticationFilter staticKeyAuthenticationFilter) {
+        this.staticKeyAuthenticationFilter = staticKeyAuthenticationFilter;
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http.addFilterBefore(new RequestValidationFilter(),
-                        BasicAuthenticationFilter.class)
-                .addFilterAfter(new AuthenticationLoggingFilter(),
+        http.addFilterAt(staticKeyAuthenticationFilter,
                         BasicAuthenticationFilter.class)
                 .authorizeHttpRequests(registry ->
                         registry.anyRequest().permitAll());
-
         return http.build();
     }
 }

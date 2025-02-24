d5c20e37e10a100c49e573592454fe9b8a764b06
chubbyhippo <mkmsacc@outlook.com>
2025-02-24 21:31:19 +0700

    Add StaticKeyAuthenticationFilter for request authorization
    Introduced a new filter to authenticate requests using a static key. The filter checks the "Authorization" header against a predefined key and returns a 401 response if the key is invalid. This enhances security by preventing unauthorized access.

---
+++ spring-security-in-action/custom-filters-static-key-authentication-filter/src/main/java/io/github/chubbyhippo/basic/StaticKeyAuthenticationFilter.java
@@ -0,0 +1,29 @@
+package io.github.chubbyhippo.basic;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class StaticKeyAuthenticationFilter implements Filter {
+    @Value("${authorization.key}")
+    private String authorizationKey;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        var httpRequest = (HttpServletRequest) servletRequest;
+        var httpResponse = (HttpServletResponse) servletResponse;
+
+        var authentication = httpRequest.getHeader("Authorization");
+
+        if (authorizationKey.equals(authentication)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+}

d72aa5452575002a667dbe58fc50b16bd4455683
ch <mkmsacc@outlook.com>
2025-02-24 12:23:36 +0700

    Refactor tests to remove Environment dependency.
    Replaced the use of the Environment class with hardcoded credentials in tests. Added a new test to validate a 400 response. This simplifies test setup and ensures clarity in expected outcomes.

--- spring-security-in-action/basic-ldap-user-details-manager-embedded/src/test/java/io/github/chubbyhippo/basic/BasicApplicationTests.java
+++ spring-security-in-action/basic-ldap-user-details-manager-embedded/src/test/java/io/github/chubbyhippo/basic/BasicApplicationTests.java
@@ -5,30 +5,34 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
 import java.util.Base64;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        args = "--spring.security.user.password=12345")
-@TestPropertySource(properties = "spring.security.user.name=matthew")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class BasicApplicationTests {
 
     @Autowired
-    private Environment environment;
-    @Autowired
     private MockMvcTester mockMvcTester;
+
+    @Test
+    @DisplayName("should return 400")
+    void shouldReturn400() {
+        mockMvcTester.get()
+                .uri("/hello")
+                .assertThat()
+                .hasStatus4xxClientError();
+    }
 
     @Test
     @DisplayName("should return hello")
     void shouldReturnHello() {
-        var userName = environment.getProperty("spring.security.user.name");
-        var password = environment.getProperty("spring.security.user.password");
+        var username = "matthew";
+        var password = "12345";
+
         var base64 = Base64.getEncoder()
-                .encodeToString("%s:%s".formatted(userName, password).getBytes());
+                .encodeToString("%s:%s".formatted(username, password).getBytes());
 
         mockMvcTester.get()
                 .uri("/hello")

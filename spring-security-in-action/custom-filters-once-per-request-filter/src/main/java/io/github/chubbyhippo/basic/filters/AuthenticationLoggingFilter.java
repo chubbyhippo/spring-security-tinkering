ed0cfa2bde246fb4298c2f69af19f953c52a94ae
chubbyhippo <mkmsacc@outlook.com>
2025-02-25 22:47:04 +0700

    Refactor AuthenticationLoggingFilter to extend OncePerRequestFilter
    Replaced implementation of Filter with OncePerRequestFilter for better handling of requests within the Spring framework. Updated method signature to override doFilterInternal and adjusted parameter types accordingly. This ensures proper execution of the filter once per request.

--- spring-security-in-action/custom-filters-once-per-request-filter/src/main/java/io/github/chubbyhippo/basic/filters/AuthenticationLoggingFilter.java
+++ spring-security-in-action/custom-filters-once-per-request-filter/src/main/java/io/github/chubbyhippo/basic/filters/AuthenticationLoggingFilter.java
@@ -2,23 +2,25 @@ package io.github.chubbyhippo.basic.filters;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-public class AuthenticationLoggingFilter implements Filter {
+public class AuthenticationLoggingFilter extends OncePerRequestFilter {
     private static final Logger log = LoggerFactory.getLogger(AuthenticationLoggingFilter.class);
 
+    @SuppressWarnings("NullableProblems")
     @Override
-    public void doFilter(ServletRequest servletRequest,
-                         ServletResponse servletResponse,
-                         FilterChain filterChain) throws IOException, ServletException {
-        var httpRequest = (HttpServletRequest) servletRequest;
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
 
-        var requestId = httpRequest.getHeader("Request-Id");
+        var requestId = request.getHeader("Request-Id");
         log.info("Successfully authenticated wit Request-Id: {}", requestId);
 
-        filterChain.doFilter(servletRequest, servletResponse);
+        filterChain.doFilter(request, response);
     }
 }

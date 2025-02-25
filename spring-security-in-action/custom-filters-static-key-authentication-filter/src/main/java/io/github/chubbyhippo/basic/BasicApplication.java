5d86caf6d1420508ac7f490a666106761cb3b6dc
ch <mkmsacc@outlook.com>
2025-02-25 13:54:43 +0700

    Exclude default UserDetailsService auto-configuration.
    This commit updates the `BasicApplication` to exclude the `UserDetailsServiceAutoConfiguration` class from auto-configuration. This ensures the application does not load the default Spring Security behavior, allowing for a custom authentication mechanism. Indentation inconsistencies in the `main` method were also fixed.

--- spring-security-in-action/custom-filters-static-key-authentication-filter/src/main/java/io/github/chubbyhippo/basic/BasicApplication.java
+++ spring-security-in-action/custom-filters-static-key-authentication-filter/src/main/java/io/github/chubbyhippo/basic/BasicApplication.java
@@ -2,13 +2,13 @@ package io.github.chubbyhippo.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class BasicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BasicApplication.class, args);
-
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BasicApplication.class, args);
+    }
 
 }

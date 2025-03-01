9d93f38a5be9a75215379ed779a80d816095e812
chubbyhippo <mkmsacc@outlook.com>
2025-03-01 17:11:59 +0700

    Refactor authentication to use UserDetailsService and PasswordEncoder
    Replaced hardcoded credentials with UserDetailsService for retrieving user details and PasswordEncoder for password validation. This improves security and aligns with Spring Security best practices. Minor exception updates and code cleanup were also applied.

--- spring-security-in-action/basic-custom-authentication-provider/src/main/java/io/github/chubbyhippo/basic/CustomAuthenticationProvider.java
+++ spring-security-in-action/basic-custom-authentication-provider/src/main/java/io/github/chubbyhippo/basic/CustomAuthenticationProvider.java
@@ -1,35 +1,43 @@
 package io.github.chubbyhippo.basic;
 
-import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public CustomAuthenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         var username = authentication.getName();
         var password = authentication.getCredentials().toString();
 
-        if ("matthew".equals(username) && "12345".equals(password)) {
+        var user = userDetailsService.loadUserByUsername(username);
+        if (passwordEncoder.matches(password, user.getPassword())) {
             return new UsernamePasswordAuthenticationToken(
                     username,
                     password,
-                    List.of());
+                    user.getAuthorities());
         } else {
-            throw new AuthenticationCredentialsNotFoundException("Invalid username or password") {
-            };
+            throw new BadCredentialsException("Something went wrong");
         }
+
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
     }
 }

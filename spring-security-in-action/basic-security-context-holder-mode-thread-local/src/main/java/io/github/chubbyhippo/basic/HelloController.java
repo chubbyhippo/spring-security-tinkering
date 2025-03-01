03be11a694dc7dcccaf946bd8fb2743e062f96df
chubbyhippo <mkmsacc@outlook.com>
2025-03-01 17:48:40 +0700

    Refactor HelloController to use method-injected Authentication.
    Replaced SecurityContextHolder usage with method parameter injection for Authentication in the `/hello` endpoint. This improves code readability, aligns with best practices, and simplifies authentication handling.

--- spring-security-in-action/basic-security-context-holder-mode-thread-local/src/main/java/io/github/chubbyhippo/basic/HelloController.java
+++ spring-security-in-action/basic-security-context-holder-mode-thread-local/src/main/java/io/github/chubbyhippo/basic/HelloController.java
@@ -1,17 +1,22 @@
 package io.github.chubbyhippo.basic;
 
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        var securityContext = SecurityContextHolder.getContext();
-        var authentication = securityContext.getAuthentication();
+//    @GetMapping("/hello")
+//    public String hello() {
+//        var securityContext = SecurityContextHolder.getContext();
+//        var authentication = securityContext.getAuthentication();
+//
+//        return "Hello, %s".formatted(authentication.getName());
+//    }
 
+    @GetMapping("/hello")
+    public String hello(Authentication authentication) {
         return "Hello, %s".formatted(authentication.getName());
     }
 }

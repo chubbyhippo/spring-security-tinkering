62ccca93f9344a586974d73bcbd733b4d9cfe609
chubbyhippo <mkmsacc@outlook.com>
2025-03-02 20:57:24 +0700

    Enable async and update security context strategy
    Added the @EnableAsync annotation to support asynchronous processing. Changed the security context strategy from MODE_THREADLOCAL to MODE_INHERITABLETHREADLOCAL to ensure the context is propagated to child threads.

--- spring-security-in-action/basic-security-context-holder-mode-inheritable-thread-local-async/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
+++ spring-security-in-action/basic-security-context-holder-mode-inheritable-thread-local-async/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
@@ -3,14 +3,16 @@ package io.github.chubbyhippo.basic;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
+@EnableAsync
 public class ProjectConfig {
 
     @Bean
     InitializingBean initializingBean() {
-        return () -> SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_THREADLOCAL);
+        return () -> SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 
 }

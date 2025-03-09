37725d0dbde1da38ba4af1fb23993c7b728a36ad
chubbyhippo <mkmsacc@outlook.com>
2025-03-09 11:49:31 +0700

    Set custom realm name in HTTP Basic authentication
    Replaced the default HTTP Basic configuration with a custom realm name ("CustomRealm") for authentication. Removed the in-memory user details service and password encoder setup as they are no longer necessary. Simplified the security configuration for clarity and maintainability.

--- spring-security-in-action/basic-custom-config-realm-name-entry-point/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
+++ spring-security-in-action/basic-custom-config-realm-name-entry-point/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
@@ -2,12 +2,7 @@ package io.github.chubbyhippo.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,33 +10,9 @@ public class ProjectConfig {
 
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
-
-        http.httpBasic(Customizer.withDefaults());
-        http.authorizeHttpRequests(registry ->
-                        registry.anyRequest().authenticated()
-//                        registry.anyRequest().permitAll()
-        );
-
-        var user = User.withUsername("matthew")
-                .password("12345")
-                .authorities("read")
-                .build();
-
-        var userDetailsService = new InMemoryUserDetailsManager(user);
-        http.userDetailsService(userDetailsService);
-
+        http.httpBasic(configurer -> configurer.realmName("CustomRealm"));
+        http.authorizeHttpRequests(registry -> registry.anyRequest().authenticated());
         return http.build();
-    }
-
-//    @Bean
-//    UserDetailsService userDetailsService() {
-//
-//        return new InMemoryUserDetailsManager(user);
-//    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
     }
 
 }

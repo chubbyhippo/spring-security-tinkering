4b2fa1b6b7201e5de5cb3e60e9a3a44e44deba2f
chubbyhippo <mkmsacc@outlook.com>
2025-03-09 19:00:10 +0700

    Switch to `@Controller` and update endpoint method.
    Replaced `@RestController` with `@Controller` to support returning views. Updated the endpoint from `/hello` to `/home`, and adjusted the method to return `home.html` instead of a plain string.

--- spring-security-in-action/form-login/src/main/java/io/github/chubbyhippo/basic/HelloController.java
+++ spring-security-in-action/form-login/src/main/java/io/github/chubbyhippo/basic/HelloController.java
@@ -1,13 +1,13 @@
 package io.github.chubbyhippo.basic;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello!";
+    @GetMapping("/home")
+    String home() {
+        return "home.html";
     }
 }

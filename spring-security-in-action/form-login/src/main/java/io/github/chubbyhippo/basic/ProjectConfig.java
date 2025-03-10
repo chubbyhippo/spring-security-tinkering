59d5501e1c0fad6969f8ed9a0e17124a77cb11b1
chubbyhippo <mkmsacc@outlook.com>
2025-03-10 21:03:58 +0700

    Set custom success URL for form login in security config
    Updated the `formLogin` configuration to redirect to `/home` as the default success URL after login. This ensures a consistent post-login user experience. Removed unused `Customizer` import for better code clarity.

--- spring-security-in-action/form-login/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
+++ spring-security-in-action/form-login/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
@@ -2,7 +2,6 @@ package io.github.chubbyhippo.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,7 +10,7 @@ public class ProjectConfig {
 
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.formLogin(Customizer.withDefaults());
+        http.formLogin(configurer -> configurer.defaultSuccessUrl("/home", true));
         http.authorizeHttpRequests(registry ->
                 registry.anyRequest().authenticated());
 

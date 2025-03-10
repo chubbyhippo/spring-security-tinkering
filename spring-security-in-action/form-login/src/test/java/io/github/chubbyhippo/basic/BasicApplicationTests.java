48ff20a43a2da6c0d236b9a91817ebe1fa83c85b
chubbyhippo <mkmsacc@outlook.com>
2025-03-10 20:45:02 +0700

    Add test for authenticated user accessing home page
    Introduced a new test to verify that an authenticated user with username "matthew" can access the "/home" page and resolve the correct view ("home.html"). This ensures that authentication and view resolution work as expected for authorized users.

--- spring-security-in-action/form-login/src/test/java/io/github/chubbyhippo/basic/BasicApplicationTests.java
+++ spring-security-in-action/form-login/src/test/java/io/github/chubbyhippo/basic/BasicApplicationTests.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -22,5 +25,15 @@ class BasicApplicationTests {
                 .assertThat()
                 .redirectedUrl()
                 .endsWith("/login");
+    }
+
+    @Test
+    @DisplayName("should return home")
+    @WithUserDetails("matthew")
+    void shouldReturnHome() {
+        mockMvcTester.get()
+                .uri("/home")
+                .assertThat()
+                .hasViewName("home.html");
     }
 }

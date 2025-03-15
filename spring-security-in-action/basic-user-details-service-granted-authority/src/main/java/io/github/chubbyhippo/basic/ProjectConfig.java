54854f1a54e3388d043540d37d3ed64b64286ecf
chubbyhippo <mkmsacc@outlook.com>
2025-03-15 21:33:06 +0700

    Refactor ProjectConfig to remove unused security imports.
    Removed unused Spring Security-related imports such as `HttpSecurity`, `Customizer`, and `SecurityFilterChain`. This simplifies the configuration and improves maintainability by eliminating unnecessary dependencies.

--- spring-security-in-action/basic-user-details-service-granted-authority/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
+++ spring-security-in-action/basic-user-details-service-granted-authority/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
@@ -2,14 +2,11 @@ package io.github.chubbyhippo.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {

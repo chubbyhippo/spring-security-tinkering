0af7c4c558efebcb7d89825bcbc791c4d1d387da
chubbyhippo <mkmsacc@outlook.com>
2025-03-15 21:32:51 +0700

    Add UserDetailsService bean and configure in-memory users
    Refactored ProjectConfig to include a UserDetailsService bean. Added two users with distinct roles (READ and WRITE) in an InMemoryUserDetailsManager for better user management. Removed unnecessary code and streamlined the configuration for clarity.

--- spring-security-in-action/basic-user-details-service-granted-authority/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
+++ spring-security-in-action/basic-user-details-service-granted-authority/src/main/java/io/github/chubbyhippo/basic/ProjectConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -14,30 +15,24 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ProjectConfig {
 
     @Bean
-    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    public UserDetailsService userDetailsService() {
+        var manager = new InMemoryUserDetailsManager();
 
-        http.httpBasic(Customizer.withDefaults());
-        http.authorizeHttpRequests(registry ->
-                        registry.anyRequest().authenticated()
-//                        registry.anyRequest().permitAll()
-        );
-
-        var user = User.withUsername("matthew")
+        var user1 = User.withUsername("matthew")
                 .password("12345")
-                .authorities("read")
+                .authorities("READ")
                 .build();
 
-        var userDetailsService = new InMemoryUserDetailsManager(user);
-        http.userDetailsService(userDetailsService);
+        var user2 = User.withUsername("mark")
+                .password("12345")
+                .authorities("WRITE")
+                .build();
 
-        return http.build();
+        manager.createUser(user1);
+        manager.createUser(user2);
+
+        return manager;
     }
-
-//    @Bean
-//    UserDetailsService userDetailsService() {
-//
-//        return new InMemoryUserDetailsManager(user);
-//    }
 
     @Bean
     PasswordEncoder passwordEncoder() {

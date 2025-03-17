a4ba6654a0556a262f75b5b093a76156bcde82d3
chubbyhippo <mkmsacc@outlook.com>
2025-03-17 19:34:53 +0700

    Add basic Spring Boot application with security integration
    This commit initializes a Spring Boot project including an HTTP endpoint (`/hello`), Basic Authentication, and user details with in-memory storage. Security is configured to restrict access based on user roles, and tests have been added to verify the functionality. Gradle is set up for build and dependency management.

---
+++ spring-security-in-action/basic-user-details-service-granted-authority-access/src/test/java/io/github/chubbyhippo/basic/BasicApplicationTests.java
@@ -0,0 +1,57 @@
+package io.github.chubbyhippo.basic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+import java.util.Base64;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class BasicApplicationTests {
+
+    @Autowired
+    private MockMvcTester mockMvcTester;
+
+    @Test
+    @DisplayName("should return 400")
+    void shouldReturn400() {
+        mockMvcTester.get()
+                .uri("/hello")
+                .assertThat()
+                .hasStatus4xxClientError();
+    }
+
+    @Test
+    @DisplayName("should return 400 with read permission")
+    @WithUserDetails("matthew")
+    void shouldReturn400WithReadPermission() {
+        mockMvcTester.get()
+                .uri("/hello")
+                .assertThat()
+                .hasStatus4xxClientError();
+    }
+
+    @Test
+    @DisplayName("should return hello")
+    void shouldReturnHello() {
+        var username = "mark";
+        var password = "12345";
+
+        var base64 = Base64.getEncoder()
+                .encodeToString("%s:%s".formatted(username, password).getBytes());
+
+        mockMvcTester.get()
+                .uri("/hello")
+                .header("Authorization", "Basic %s".formatted(base64))
+                .assertThat()
+                .hasStatusOk()
+                .doesNotHaveFailed()
+                .hasContentType("text/plain;charset=UTF-8")
+                .hasBodyTextEqualTo("Hello!");
+    }
+}
